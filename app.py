e6ccb3d60c5bb8f41eb4433b21052e4d3768a8cb
Chen Liang <chen.albert.liang@gmail.com>
2021-05-06 23:54:22 -0400

    revised app.py

--- app.py
+++ app.py
@@ -7,13 +7,12 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'Fuck this shit'
-db.init_app(app)  # You need to call  db.init_app(app) before you try to interact with database. Not mentioned in the video
+# db.init_app(app)  # You need to call  db.init_app(app) before you try to interact with database. Not mentioned in the video
 api = Api(app)
 
 @app.before_first_request
@@ -30,5 +29,6 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
