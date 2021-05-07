fd69c0f561b0689b423bc4981c25a0a7c11e3d12
Chen Liang <chen.albert.liang@gmail.com>
2021-05-07 00:53:21 -0400

    Address cannot load plugin postgres issue by postgresql

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from resources.store import Store, StoreList
 
    
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL2', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'Fuck this shit'
 # db.init_app(app)  # You need to call  db.init_app(app) before you try to interact with database. Not mentioned in the video
